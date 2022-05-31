464edfd94d580f2ae5684e2347d70ccf9ad58ca7
hugoherculano <hugohercullano@gmail.com>
2022-05-31 11:59:43 -0300

    Add para webService

--- src/Service/Api.js
+++ src/Service/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Api = axios.create({
-    baseURL: 'https://yt.idcoude.com.br'
+    baseURL: 'https://hun.idcoude.com.br'
     //baseURL: 'http://localhost:8000'
 });
 
